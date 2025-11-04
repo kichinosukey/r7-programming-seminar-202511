8c0f292cef3892f79ed2965e9938b59bf0ea0f4e
kichinosukey <kichinosuke.fukuhara@gmail.com>
2025-11-04 16:29:07 +0900

    Uの字コースを走るリファレンス実装。直進 -> 障害物検知したら停止 -> 右に旋回 -> 直進 -> 右に旋回 -> 直進

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function () {
-	
-})
+while (maqueen.Ultrasonic() > 5) {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+}
+maqueen.motorStop(maqueen.Motors.All)
+maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+basic.pause(1000)
+maqueen.motorStop(maqueen.Motors.All)
+while (maqueen.Ultrasonic() > 5) {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+}
+maqueen.motorStop(maqueen.Motors.All)
+maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+basic.pause(1000)
+while (maqueen.Ultrasonic() > 5) {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+}
